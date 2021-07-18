5ee12a3716d4d15af3b7a7d5bfdd2ed9485c7453
Karuna7 <karunkaruna72@gmail.com>
2021-07-17 21:56:46 -0700

    added a script

---
+++ pratice1.py
@@ -0,0 +1,5 @@
+#Create an array A = [1,2,3,4,5]. Print all elements greater than 2.
+a=[1,2,3,4,5]
+for i in range(len(a)):
+	if i>2: 
+		print(i)
---
+++ pratice2.py
@@ -0,0 +1,6 @@
+#Take the input for a number from user. Check if the number is divisible by 3. Print "yes" if it is.
+x=input("Please enter a number: ")
+a=int(x)
+if a%3==0:
+	print("yes")
+
